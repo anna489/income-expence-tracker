f4d1e348d8c1759ac8f10288fd60b30d88b9cf93
anar mac <anarganbaatar@Anars-MacBook-Air.local>
2023-12-05 18:36:52 +0800

    getrequest hiiv

--- BackEnd/controller/authTransaction.js
+++ BackEnd/controller/authTransaction.js
@@ -3,14 +3,48 @@ const { sql } = require("../config/pgDb");
 const transaction = async (req, res) => {
   try {
     const { user_id, name, amount, description, category_id } = req.body;
-    await sql`INSERT INTO transaction( name, amount, description,category_id,user_id) 
+    await sql`INSERT INTO transaction( name, amount, description, category_id, user_id) 
     VALUES(${name}, ${amount}, ${description} , ${category_id}, ${user_id})`;
     res.status(201).json({ message: "success" });
   } catch (error) {
     res.status(500).json({ message: error });
   }
 };
-//  transaction_type, description, category_id
-// ${transaction_type}, ${description}, ${category_id},
-// transaction_type, description, category_id
-module.exports = { transaction };
+
+
+const gettransaction = async (req, res) => {
+  try {
+    const { user_id } = req.body;
+    const findTrans= 
+    await sql`SELECT user_id, name FROM transaction WHERE  user_id=${user_id}`;
+
+    if (findTrans.length === 0) {
+      return res.status(400).json({ message: "user not found" });
+    }
+    res.status(201).json({ message: "success" , user_id});
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
+
+
+const puttransaction = async (req, res) => {
+  try {
+    const { user_id, name, amount, description } = req.body;
+
+    const findTrans= 
+    await sql`SELECT user_id, name FROM transaction WHERE  user_id=${user_id}`;
+    if (findTrans.length === 0) {
+      return res.status(400).json({ message: "user not found" });
+    }
+
+    await sql`UPDATE INTO transaction( name, amount, description) 
+    VALUES(${name}, ${amount}, ${description} )`;
+    res.status(201).json({ message: "success" });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
+
+
+module.exports = { transaction , gettransaction , puttransaction};
--- BackEnd/routes/transacRouter.js
+++ BackEnd/routes/transacRouter.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const { transaction } = require("../controller/authTransaction");
-
+const {gettransaction}=require ("../controller/authTransaction");
+const {puttransaction} =require("../controller/authTransaction")
 const router = Router();
 
 router.route("/transaction").post(transaction);
+router.route("/gettransaction").get(gettransaction);
+router.route("/transaction").delete(transaction);
+router.route("/transaction").put(puttransaction);
 
 module.exports = router;
